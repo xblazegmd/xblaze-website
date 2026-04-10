bc72a4033f4f2e733e74ae7b36136a30966da1a1
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-04-10 09:54:58 -0500

    better logs and fix typo

--- src/routes/patreon/tier/+server.ts
+++ src/routes/patreon/tier/+server.ts
@@ -6,8 +6,11 @@ import { supabase } from "$lib/membersDatabase.js";
 export async function GET({ url }) {
     const accountID = url.searchParams.get("accountID");
     if (!accountID) {
+        console.error("Missing parameter: 'accountID'");
         throw httpError(400, "Missing parameter: 'accountID'");
     }
+
+    console.log("Fetching Patreon access token from database...");
 
     const { data, error } = await supabase
         .from("members")
@@ -15,8 +18,12 @@ export async function GET({ url }) {
         .eq("account_id", Number(accountID));
 
     if (error) {
+        console.error(`Could not fetch info from database: ${error.message}`);
         throw httpError(500, `Could not fetch info from database: ${error.message}`);
     }
+
+    console.log("Fetched Patreon access token from database");
+    console.log("Fetching Patreon tier info...");
 
     const reqUrl = new URL("https://www.patreon.com/api/oauth2/v2/identity?include=memberships.currently_entitled_tiers&"
         + encodeURIComponent("fields[member]") + "=patron_status&" + encodeURIComponent("fields[tier]") + "=title"
@@ -28,33 +35,42 @@ export async function GET({ url }) {
 
     if (!req.ok) {
         const message = await req.text();
-        console.error(message);
-        throw httpError(500, `${req.status}`)
+        console.error(`Could not request Patreon API endpoint '/api/oauth2/v2/identity' (${req.status}): ${message}`);
+        throw httpError(500, `Could not request Patreon API endpoint '/api/oauth2/v2/identity': ${req.status}`)
     }
+
+    console.log("Fetched Patreon tier info");
 
     const jsonRes = await req.json();
     console.log(JSON.stringify(jsonRes));
 
     // bro hasn't even joined :sob:
-    if (jsonRes["data"]["relationships"]["memberships"]["data"].lenght === 0) {
+    if (jsonRes["data"]["relationships"]["memberships"]["data"].length === 0) {
+        console.log("blud hasn't even joined the patreon ;-;");
         return json({ tier: 0 });
     }
 
     const patronStatus = jsonRes["included"][0]["attributes"]["patron_status"];
 
     if (!patronStatus || patronStatus !== "active_patron") {
+        console.log("Not an active patron");
         return json({ tier: 0 });
     }
 
     const tierTitle = jsonRes["included"][1]["attributes"]["title"];
 
     if (tierTitle === "Free") {
+        console.log("Free tier");
         return json({ tier: 0 });
     } else if (tierTitle === "Plain Normal Supporter Tier™") {
+        console.log("Plain Normal Supporter Tier");
         return json({ tier: 1 });
     } else if (tierTitle === "Amazing Beautiful Crab Tier™") {
+        console.log("Amazing Beautiful Crab Tier");
         return json({ tier: 2 });
     }
+
+    console.error("THIS SHOULD BE UNREACHABLE!!!!");
 
     return json({ tier: 0 });
 }
