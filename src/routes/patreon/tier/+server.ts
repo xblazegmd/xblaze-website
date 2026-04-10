9e95cf808d6906a286f05b9975b1172672ae5e8b
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-04-10 00:17:28 -0500

    idk what im doiiiiiiiinnnng

---
+++ src/routes/patreon/tier/+server.ts
@@ -0,0 +1,36 @@
+import { error as httpError, json } from "@sveltejs/kit";
+
+import { supabase } from "$lib/membersDatabase.js";
+
+export async function GET({ url }) {
+    const accountID = url.searchParams.get("accountID");
+    if (!accountID) {
+        throw httpError(400, "Missing parameter: 'accountID'");
+    }
+
+    const { data, error } = await supabase
+        .from("members")
+        .select("patreon_access_token")
+        .eq("account_id", Number(accountID));
+
+    if (error) {
+        throw httpError(500, `Could not fetch info from database: ${error.message}`);
+    }
+
+    const reqUrl = new URL("https://www.patreon.com/api/oauth2/v2/identity");
+    reqUrl.searchParams.set("include", "memberships.currently_entitled_tiers,memberships.campaign");
+    reqUrl.searchParams.set(encodeURIComponent("fields[user]"), "patron_status");
+    reqUrl.searchParams.set(encodeURIComponent("fields[tier]"), "title");
+
+    const req = await fetch(reqUrl.href, {
+        headers: { "Authorization": `Bearer ${data[0]}` }
+    });
+
+    if (!req.ok) {
+        throw httpError(500, `${req.status}`)
+    }
+
+    const stuff = await req.json();
+    console.log(stuff.toString());
+    return json({ ok: true });
+}
