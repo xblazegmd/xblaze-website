9850362602922c5de8f772a7228b8964b9d6c929
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-09-05 17:02:57 -0500

    RIP patreon endpoints

--- src/lib/membersDatabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../../supabase.ts";
-
-import { SUPABASE_URL, SUPABASE_KEY } from "$env/static/private";
-
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
--- src/routes/patreon/link/cancel/+server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function GET({ cookies }) {
-    cookies.delete("patreonAccessToken", { path: "/" });
-    cookies.delete("patreonRefreshToken", { path: "/" });
-    cookies.delete("patreonExpiresIn", { path: "/" });
-    throw redirect(302, "/patreon/link");
-}
